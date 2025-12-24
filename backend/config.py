62f33241c6c39c63a08928bd73577c891ad5d4fe
Brain-Ripe <watashinosoulsociety0210@gmail.com>
2025-12-25 01:58:42 +0530

    Add your own keys and DB Creds

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 from urllib.parse import quote_plus
 
-DB_USER = "root"
-DB_PASSWORD = quote_plus("Anaklusmos142") 
+DB_USER = "root" #change if username is diff
+DB_PASSWORD = quote_plus("")  #put password for mariadb here
 DB_HOST = "localhost"
 DB_NAME = "student_event_system"
 
